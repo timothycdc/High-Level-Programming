7a012a0bb27f36e5f240f0d590179787677736be
timothycdc <timothycdc@gmail.com>
2024-02-05 00:52:51 +0000

    error fixes

--- session3/04_pastpaper.fs
+++ session3/04_pastpaper.fs
@@ -69,7 +69,8 @@ let q6 x y = x <| x y
 let q7 = [ 1, [] ]
 // q7: list<int * list<’a>>
 
-let q8 = id q7
+// let q8 = id q7
+//  list <int * list<obj>>
 // passing q7 through id implicitly involves the type system
 // id is generic and doesn't impose additional type constraints
 // however, the way type inf wokrs in F# can lead to generalisations,
@@ -94,8 +95,12 @@ let q8 = id q7
 //  functions can do all sorts of things with their inputs: storing them
 //  in mutable variables, or pass them to other functions with stricter type reqs
 
+// value restriction
+// Either define 'q8' as a simple data term, make it a function with explicit arguments or, if you do not intend for it to be generic, add a type annotation.
+
+
 let q9' = None
-let q9 = 1 + Option.get q9'
+// let q9 = 1 + Option.get q9'
 
 let q10' = []
 
@@ -105,21 +110,27 @@ let q10'' = q10 @ [ 1 ]
 
 let q11 b x y z = if b then x y else x z
 
-type q12 = unit option option
+// type Q12 = unit option option
 // unit is a type with only one value, ()
 // option is a type with two values, Some and None
 // number of distinct values of that type: 3
 //  unit, unit option, unit option option
 
-type Q13 = Result<Unit, bool> * Result<bool -> bool -> bool, bool -> Unit>
+// type Q13 = Result<Unit, bool> * Result<bool -> bool -> bool, bool -> Unit>
 // Q13 is a tuple of two types
-// number of distinct values of that type: 3*(8+2) = 30
+// number of distinct values of that type: 3*(16+1) = 51
 // Result<Unit, bool> has 3 values: Ok (), Error true, Error false
+
 // Result<bool -> bool -> bool, bool -> Unit> can be broken down into
 // Ok bool -> bool -> bool, Error bool -> Unit
-// The first Ok has 8 possible values True or False chained in three, 2^3
-// The second Error has 2 possible values, True -> () or False -> ()
-// 8+2 = 10, 3*10 = 30
+// The first Ok has 16 possible values True or False
+//  bool -> bool -> bool is a function that takes 2 bools and returns a bool
+// there are 4 possible values for input (00, 01, 10, 11)
+// and there are 2 possible values for output (0, 1)
+//  2^4 distinct functions, = 16
+// The second Error has 1 possible distinct function, 1, just return unit () = 1
+// 16 + 1 = 17
+// 17 * 3 = 51
 
 
 // suppose |'A|=a, |'B| = b
@@ -132,14 +143,16 @@ type Q14<'A, 'B> = Result<Result<'A, 'B>, Option<Result<'A, 'B>>> * bool
 
 let q15 = 6 |> (*) 5 |> (+) ((*) 3 4) |> (-) 0
 
-// 42
+// -42
 
 let q16 =
     [ 0..10 ]
     |> List.partition (fun n -> n % 3 = 0)
-    // splits at 3
+    // first half of the tuple is a list of numbers divisible by 3
+    // second half is a list of numbers not divisible by 3
+    //  ([0; 3; 6; 9], [1; 2; 4; 5; 7; 8; 10])
     |> fun (a, b) -> List.rev a @ b
-// [3;2;1;4;5;6;7;8;9;10]
+// [9; 6; 3; 0; 1; 2; 4; 5; 7; 8; 10]
 
 let q17 =
     let thing x =
@@ -148,6 +161,42 @@ let q17 =
         | _ -> 4
 
     thing 3 1
-// 4
-// how this works: the first x is a pattern match, the second x is a guard
-// feed in 3,
+// 2
+// thing x gets the 3
+// function gets the 1
+// function is a shorthand for fun x -> match x with,
+// defining a function that immediately pattern matches on its input arg
+// pretty unclear, best to see q17'
+let example =
+    function
+    | 0 -> "zero"
+    | _ -> "other"
+
+let example' x =
+    match x with
+    | 0 -> "zero"
+    | _ -> "other"
+
+let q17' =
+    let thing x y =
+        match y with
+        | y when y = 1 -> 2
+        | _ -> 4
+
+    thing 3 1
+
+
+
+let q18 =
+    let double f x = f (f x)
+    let f a b c d = (a b) (c d)
+    let g = f double id double double
+
+    g (fun x -> x + 1) 3
+
+
+// (double id) (double double) (fun x -> x + 1) 3
+// (double) (double double) (fun x -> x + 1) 3
+// (double double double) (fun x -> x + 1) 3
+// f(f(f(f(f(f(f(f x))))))) (fun x -> x + 1) 3
+// 3 + 8x1 = 11
